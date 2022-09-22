2e84d61b3f6b459d8df315e1480ba97f266aa6c9
Nick <efutin.kolya@yandex.ru>
2022-09-22 10:23:55 +0300

    домашка 4

---
+++ Program4.cs
@@ -0,0 +1,12 @@
+﻿ Console.WriteLine ("Дай мне три числа");
+  int number1 = Convert.ToInt32 (Console.ReadLine());
+  int number2 = Convert.ToInt32 (Console.ReadLine());
+  int number3 = Convert.ToInt32 (Console.ReadLine());
+            
+    if (number1 > number2 && number1 > number3)
+        Console.WriteLine("Nubmer Max = " + number1); 
+    else if (number2 > number3)
+        Console.WriteLine("Number Max = " + number2); 
+    else
+        Console.WriteLine("Number Max = " + number3); 
+                  
